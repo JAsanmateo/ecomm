574367f1455409729c76e36cfc3ae92a5ba01f99
Jose San Mateo <info@joseasanmateo.com>
2022-09-14 20:45:20 -0700

    Finished up making the database models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -5,7 +5,16 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
+  {id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    //how to make this reference another category
+  }
     // define columns
   },
   {
